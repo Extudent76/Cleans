90cea8104926876c37eb385110b8f420dc29dcfe
Extudent <pivkinalexandr17@gmail.com>
2023-05-23 15:20:49 +0300

    Gallery Animation

--- js/app.js
+++ js/app.js
@@ -7,4 +7,42 @@ ScrollSmoother.create({
 	effects: true
 })
 
+gsap.fromTo( '.gallery-top-left', { opacity: 0, x: -500 }, {
+	opacity: 1, x: 0,
+	scrollTrigger: {
+		trigger: '.gallery-top-left',
+		start: '-1000',
+		end: 'top',
+		scrub: true
+	}
+})
 
+gsap.fromTo( '.gallery-bottom-left', { opacity: 0, x: -440 }, {
+	opacity: 1, x: 0,
+	scrollTrigger: {
+		trigger: '.gallery-bottom-left',
+		start: '-1500',
+		end: '-100',
+		scrub: true
+	}
+})
+
+gsap.fromTo( '.gallery-top-right', { opacity: 0, x: 500 }, {
+	opacity: 1, x: 0,
+	scrollTrigger: {
+		trigger: '.gallery-top-right',
+		start: '-1000',
+		end: 'top',
+		scrub: true
+	}
+})
+
+gsap.fromTo( '.gallery-bottom-right', { opacity: 0, x: 440 }, {
+	opacity: 1, x: 0,
+	scrollTrigger: {
+		trigger: '.gallery-bottom-right',
+		start: '-1500',
+		end: '-100',
+		scrub: true
+	}
+})
